182dc7b6d4c3bdcd23fdbb399a6e9775f07a2415
vishwanathvishu95 <68235923+vishwanathvishu95@users.noreply.github.com>
2020-07-16 21:33:55 +0530

    Assignment3

---
+++ three.js
@@ -0,0 +1,9 @@
+var age = prompt("Please enter your age");
+
+if(age >= 21 ) {
+    alert("Can drink");
+} else if (age < 21 ) {
+    alert("Cannot Drink");
+} else { 
+    alert("Please enter an age");
+}
